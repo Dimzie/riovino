efb3375d754ee1824c6c90ae1324f9abbbdaea6d
Oleksandr FIlippov <filya170895@gmail.com>
2024-03-24 12:25:31 +0100

    added all operations to authSlice

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import { register } from './operations';
+import { getCurrent, login, logout, register } from './operations';
 
 const handlePending = state => {
   state.isLoading = true;
@@ -28,8 +28,17 @@ const authSlice = createSlice({
     builder
       // PENDING
       .addCase(register.pending, handlePending)
+      .addCase(login.pending, handlePending)
+      .addCase(logout.pending, handlePending)
+      .addCase(getCurrent.pending, state => {
+        state.isRefreshing = true;
+      })
       // REJECTED
       .addCase(register.rejected, handleRejected)
+      .addCase(login.rejected, handleRejected)
+      .addCase(getCurrent.rejected, state => {
+        state.isRefreshing = false;
+      })
       // FULFILLED
       .addCase(register.fulfilled, (state, { payload }) => {
         state.user = payload.user;
@@ -37,6 +46,27 @@ const authSlice = createSlice({
         state.isLoggedIn = true;
         state.isLoading = false;
         state.error = null;
+      })
+      .addCase(login.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.rioAccessToken = payload.accessToken;
+        state.isLoggedIn = true;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(logout.fulfilled, state => {
+        state.user = {};
+        state.rioAccessToken = null;
+        state.isLoggedIn = false;
+        state.isRefreshing = false;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(getCurrent.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.rioAccessToken = payload.accessToken;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
       });
   },
 });
