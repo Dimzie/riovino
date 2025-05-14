16c738240b8825b54e8a23da88aa1de2c3d2c417
Oleksandr FIlippov <filya170895@gmail.com>
2025-05-14 15:16:09 +0200

    load more
    added load more btn and logic to it

--- src/components/Lists/TypeList/TypeList.jsx
+++ src/components/Lists/TypeList/TypeList.jsx
@@ -1,19 +1,26 @@
 import BackBtn from 'components/BackBtn/BackBtn';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useLocation, useParams } from 'react-router-dom';
 import { getProducts } from '../../../redux/products/operations';
 import { useProducts } from 'hooks/useProducts';
 import TypeItem from 'components/Items/TypeItem/TypeItem';
-import { H2Title, List } from './TypeList.styled';
+import { BackBtnSpan, H2Title, List } from './TypeList.styled';
 import { allSubTypes } from 'data/data';
 
 const TypeList = () => {
+  const [page, setPage] = useState(1);
   const location = useLocation();
   const backLink = location?.state?.from ?? '/';
   const dispatch = useDispatch();
   const { subType } = useParams();
   const { isProductsLoading, products } = useProducts();
+
+  const loadMore = () => {
+    if (!isProductsLoading) {
+      setPage(prev => prev + 1);
+    }
+  };
 
   const getSubTypeByParams = subType => {
     const result = allSubTypes.find(type => type.href === subType);
@@ -23,35 +30,35 @@ const TypeList = () => {
 
   useEffect(() => {
     const result = getSubTypeByParams(subType);
-    dispatch(getProducts(result.id));
-  }, [dispatch, subType]);
+    dispatch(
+      getProducts({
+        subType: result.id,
+        page,
+      })
+    );
+  }, [dispatch, page, subType]);
 
   return (
     <>
       <H2Title>{result.title}</H2Title>
-      {!isProductsLoading && (
-        <List>
-          {products.products.map(
-            ({
-              id,
-              name,
-              price,
-              stock,
-              desc
-            }) => {
-              return (
-                <TypeItem
-                  key={id}
-                  name={name}
-                  price={price}
-                  id={id}
-                  stock={stock}
-                  desc={desc}
-                />
-              );
-            }
-          )}
-        </List>
+      <List>
+        {products.products.map(({ id, name, price, stock, desc }) => {
+          return (
+            <TypeItem
+              key={id}
+              name={name}
+              price={price}
+              id={id}
+              stock={stock}
+              desc={desc}
+            />
+          );
+        })}
+      </List>
+      {products.products.length !== products.totalProducts && (
+        <BackBtnSpan onClick={loadMore}>
+          {isProductsLoading && page !== 1 ? 'LOADING' : 'LOAD MORE'}
+        </BackBtnSpan>
       )}
       <BackBtn backLink={backLink} />
     </>
